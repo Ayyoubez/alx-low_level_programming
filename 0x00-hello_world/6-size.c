1b143951984e50facdd2b9105462334aa513d46c
Ayyoubez <ayyoubez@gmail.com>
2023-03-16 12:59:53 -0700

    m

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,13 @@ int main(void)
 	int a;
 	char b;
 	long l;
+	long long d;
 	float f;
 
-	printf("Size of a char :%c.\n", sizeof(a));
-	printf("Size of an int :%d.\n", sizeof(b));
-	printf("Size of a long :%l.\n", sizeof(l));
-	printf("Size of a float :%f.\n", sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 	return (0);
 }
