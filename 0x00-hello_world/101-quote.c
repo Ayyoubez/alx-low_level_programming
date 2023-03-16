9b1434ea7a672604b7ee1b6c19fce6cd15d41c63
Ayyoubez <ayyoubez@gmail.com>
2023-03-16 14:47:55 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,10 @@
 #include <unistd.h>
-#include <stdio.h>
 /**
  * main - -main function
  * Return: 1(success)
  */
 int main(void)
 {
-	const char message[] = "and that piece of art is useful\" - Dora Korpar,
-	      2015-10-19\n";
-
-	const size_t length = sizeof(message) - 1;
-
-	ssize_t bytes_written = write(STDERR_FILENO, message, length);
-
-	if (bytes_written != length)
-	return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
