f49e4b0460499a581ad154c8a16e0b25cc97bf0d
Ayyoubez <ayyoubez@gmail.com>
2023-03-23 11:18:06 -0700

    Debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * positive_or_negative number
+ */
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	if (i == 0)
+	{
+	printf("%d is zero\n", i);
+	}
+}
