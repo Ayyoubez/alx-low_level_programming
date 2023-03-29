bb1de0e7213e4b7b4e562e201294a64854dcbf91
Ayyoubez <ayyoubez@gmail.com>
2023-03-29 13:55:33 -0700

    strncat logic

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,12 +15,14 @@ char *_strncat(char *dest, char *src, int n)
 
 	while (dest[i] != '\0')
 	{
-		j++;
 		i++;
 	}
-	for (i = 0; i < n; i++)
+	while (j < n && src[j] != '\0')
 	{
-		dest[j+i] = src[i];
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
+	dest[i] = '\0';
 return (dest);
 }
