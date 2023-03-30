7aa5d0b7171a3cc6840d32a87fe755b3305ed196
Ayyoubez <ayyoubez@gmail.com>
2023-03-30 11:13:29 -0700

    LEET

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*leet - leet function
+*@str: string to process
+*Return: return result
+*/
+char *leet(char *str)
+{
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == s1[j])
+			{
+				str[i] = s2[j];
+			}
+		}
+	}
+	return (str);
+}
