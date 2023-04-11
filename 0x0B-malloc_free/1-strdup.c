40435b3e48cd739e640fddd9a4432840cd0b2480
Ayyoubez <ayyoubez@gmail.com>
2023-04-11 07:37:49 -0700

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,14 +9,14 @@ char *_strdup(char *str)
 {
 	char *new;
 	unsigned int i;
-	int r = 1;
+	int r = 0;
 
 	if (str == NULL)
 		return (NULL);
 
 	for (i = 0; str[i] != '\0'; i++)
 		r++;
-	new = malloc(sizeof(char) * r);
+	new = malloc(sizeof(char) * (r + 1);
 	i = 0;
 	while (str[i] != '\0')
 	{
