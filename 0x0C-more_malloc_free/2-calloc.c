b5475bf945287b2e26343a4d2861ff530e852f61
Ayyoubez <ayyoubez@gmail.com>
2023-04-13 11:01:02 -0700

    m

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,22 @@
 #include <stdlib.h>
 #include "main.h"
 /**
+*_memset - set memory to 0
+*@s: paramemeter
+*@e: parameter
+*@n: parameter
+*Return: pointer reset
+*/
+
+int *_memset(int *s, int e, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = e;
+	return (s);
+}
+/**
 *_calloc - function to allocate memory for an array
 *@nmemb: number of elements
 *@size: size of the type
@@ -9,17 +25,13 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *p;
-	unsigned int i = 0;
+	int *p;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(nmemb * sizeof(size));
 	if (p == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
-	{
-		p[i] = '0';
-	}
+	_memset(p, 0, nmemb * size);
 return (p);
 }
