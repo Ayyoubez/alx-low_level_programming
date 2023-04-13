d12aa6303e7f3d91c2e89fbe039294a086993fee
Ayyoubez <ayyoubez@gmail.com>
2023-04-13 09:41:10 -0700

    allocation of two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include "main.h"
 /**
-*string_nconcat - a;;pcate memory for  two strings
+*string_nconcat - allocate memory for  two strings
 *@s1: string destination
 *@s2: string source
 *@n: parameter of number of bits
@@ -12,34 +12,32 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *p;
 	unsigned int i = 0;
 	unsigned int j = 0;
+	unsigned int c = 0;
+	unsigned int l = 0;
 
-/*	if (s1 == NULL || s2 == NULL)
-	{
-		s1 = "";
-		s2 = "";
-	}*/
-	while (s1[i] != '\0')
-	{
+	while (s1[i])
 		i++;
-	}
-	while (s2[j] != '\0')
-	{
+	while (s2[j])
 		j++;
-	}
 	if (n >= j)
 		p = malloc(sizeof(char) * (i + j + 1));
-	p = malloc(sizeof(char) * (i + n + 1));
+	else
+		p = malloc(sizeof(char) * (i + n + 1));
 	if (!p)
 		return (NULL);
-	for (i = 0; s1[i] != '\0'; i++)
+	while (c < i)
 	{
-		p[i] = s1[i];
+		p[c] = s1[c];
+		c++;
 	}
-	for (j = 0; j < n; j++)
+	while (n >= j && c < (i + j))
 	{
-		p[i] = s2[j];
-		i++;
+		p[c++] = s2[l++];
 	}
-	p[i] = '\0';
+	while (n < j && c < (i + n))
+	{
+		p[c++] = s2[l++];
+	}
+	p[c] = '\0';
 return (p);
 }
