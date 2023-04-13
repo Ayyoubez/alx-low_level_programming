c84c8c84f49a056cf8270fc4b4d63727b86ae59d
Ayyoubez <ayyoubez@gmail.com>
2023-04-13 16:32:02 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include "main.h"
 /**
-*
-*
-*
-*
-*
+*_realloc - resize a pointer
+*@ptr: parameter
+*@old_size: parameter
+*@new_size: parameter
+*Return: new pointer with nez size
 */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
@@ -25,16 +25,16 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (malloc(new_size));
 	new = malloc(new_size);
 	if (!new)
-		return(NULL);
+		return (NULL);
 	old = ptr;
 	if (new_size > old_size)
 	{
-		for(i = 0; i < old_size; i++)
+		for (i = 0; i < old_size; i++)
 			new[i] = old[i];
 	}
 	if (new_size < old_size)
 	{
-		for(i = 0; i < new_size; i++)
+		for (i = 0; i < new_size; i++)
 			new[i] = old[i];
 	}
 	free(ptr);
