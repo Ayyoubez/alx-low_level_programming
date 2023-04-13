80547c9fc5a1c715dcd33f87395b4e235867375f
Ayyoubez <ayyoubez@gmail.com>
2023-04-13 16:27:17 -0700

    100 realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+*
+*
+*
+*
+*
+*/
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *old;
+	char *new;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (!ptr)
+		return (malloc(new_size));
+	new = malloc(new_size);
+	if (!new)
+		return(NULL);
+	old = ptr;
+	if (new_size > old_size)
+	{
+		for(i = 0; i < old_size; i++)
+			new[i] = old[i];
+	}
+	if (new_size < old_size)
+	{
+		for(i = 0; i < new_size; i++)
+			new[i] = old[i];
+	}
+	free(ptr);
+return (ptr);
+}
