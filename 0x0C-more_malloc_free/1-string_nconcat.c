fa800c7e2bb3ddddaf42dfbd236c87c2b43639f5
Ayyoubez <ayyoubez@gmail.com>
2023-04-13 05:03:18 -0700

    nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,6 +29,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= j)
 		p = malloc(sizeof(char) * (i + j + 1));
 	p = malloc(sizeof(char) * (i + n + 1));
+	if (!p)
+		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		p[i] = s1[i];
