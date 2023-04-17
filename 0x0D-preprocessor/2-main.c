078b594a035ccdfc80d13fe9543d0ab8c7a2ab3e
Ayyoubez <ayyoubez@gmail.com>
2023-04-17 03:57:04 -0700

    preprocessor tasks

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - main function of the program
+*Return: 0 with success
+*/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+return (0);
+}
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef 0-SUM_H
+#define 0-SUM_H
+
+#define SUM(x, y)(x + y)
+
+#endif
