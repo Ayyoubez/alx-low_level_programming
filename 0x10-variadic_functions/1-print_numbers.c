df64ef44bad30349945c8e1cde784b817ead151f
Ayyoubez <ayyoubez@gmail.com>
2023-04-20 10:24:42 -0700

    second task

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		x = va_arg(args, int);
 		printf("%d", x);
-		if ( separator != NULL)
+		if (i != (n - 1) && separator != NULL)
 		printf("%c", *separator);
 	}
 	va_end(args);
