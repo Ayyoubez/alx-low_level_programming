7e75e766e823a48a762a7c78d9201eea7e5f46aa
Ayyoubez <ayyoubez@gmail.com>
2023-04-27 13:25:24 -0700

    first task

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+*print_list - function to print all elements
+*@h: parameter
+*Return: result
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t p = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0](nil)\n");
+		else
+			printf("[%u]%s\n", h->len, h->str);
+		h = h->next;
+		p++;
+	}
+return (p);
+}
