796a101dcdf5eece723ec003e69d4bdd5975dbd6
Ayyoubez <ayyoubez@gmail.com>
2023-04-27 15:48:46 -0700

    5th task

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+*free_list - free list
+*@head: parameter
+*Return: nothing
+*/
+void free_list(list_t *head)
+{
+	list_t *t;
+
+	while (*head)
+	{
+		t = head->next;
+		free(head->str);
+		free(head);
+		head = t;
+	}
+}
