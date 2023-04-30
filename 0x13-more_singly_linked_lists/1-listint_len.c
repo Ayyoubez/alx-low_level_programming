3bcc7687abad45c136f5847efbc479268c0c45eb
Ayyoubez <ayyoubez@gmail.com>
2023-04-30 01:22:18 -0700

    second task

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,12 +2,18 @@
 
 
 /**
-*
-*
-*
-*
-*
+*listint_len - counting the number of nodes
+*@h: parameter
+*Return: number of nodes
 */
 size_t listint_len(const listint_t *h)
 {
-	
+	size_t c = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		c++;
+	}
+return (c);
+}
