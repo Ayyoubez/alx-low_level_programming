5bdd6bc9e4f33df8d0e2733170bd550f74109db1
Ayyoubez <ayyoubez@gmail.com>
2023-04-30 23:15:33 -0700

    pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+*pop_listint - delete the first node
+*@head: parameter
+*Return: data stored in node deleted
+*/
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int x = 0;
+
+	if (head == NULL)
+		return (0);
+	tmp = *head;
+	*head = (*head)->next;
+	x = tmp->n;
+return (x);
+}
