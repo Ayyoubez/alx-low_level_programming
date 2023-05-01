2ead31c1b92f7b635fc19024a24a174948e38ef6
Ayyoubez <ayyoubez@gmail.com>
2023-04-30 21:47:19 -0700

    free listing

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,9 +6,9 @@
 */
 void free_listint2(listint_t **head)
 {
+	if (head == NULL || *head == NULL)
+		return;
 	listint_t *tmp;
-
-	tmp = (listint_t *)malloc(sizeof(listint_t));
 
 	while (*head != NULL)
 	{
