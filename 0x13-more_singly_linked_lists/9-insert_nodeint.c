5769eb2b7459ec8354df06453a5fe46c7a278a66
Ayyoubez <ayyoubez@gmail.com>
2023-05-01 01:39:15 -0700

    new node insertion

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+/**
+*insert_nodeint_at_index - insert a new node
+*@head: parameter
+*@idx: parameter
+*@n: parameter
+*Return: result
+*/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *newnode, *tmp;
+	unsigned int i = 0;
+
+	newnode = (listint_t *)malloc(sizeof(listint_t));
+	if (newnode == NULL || head == NULL)
+		return (NULL);
+	newnode->n = n;
+	newnode->next = NULL;
+	if (idx == 0)
+	{
+		newnode->next = *head;
+		*head = newnode;
+		return (newnode);
+	}
+	tmp = *head;
+	while (i < idx - 1 && tmp != NULL)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	if (tmp != NULL)
+	{
+		newnode->next = tmp->next;
+		tmp->next = newnode;
+		return (NULL);
+	}
+	free(newnode);
+	return (NULL);
+}
