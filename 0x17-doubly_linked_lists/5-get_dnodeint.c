dddedc7db4e061b5e313a1dfb35a49ca726748c0
ayyoubez <ayyoubez@gmail.com>
2024-01-11 22:52:01 -0800

    doubly linked list

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+* get_dnodeint_at_index - value at index
+* @head: head pointer
+* @index: index of the node
+* Return: return a node at index
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *temp;
+	unsigned int count = 1;
+	unsigned int i = 1;
+
+	temp = head;
+	if (head == NULL)
+		return (NULL);
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+		count++;
+	}
+	if (index > count)
+		return (NULL);
+	temp = head;
+	while (i < index)
+	{
+		temp = temp->next;
+		i++;
+	}
+	return (temp);
+}
