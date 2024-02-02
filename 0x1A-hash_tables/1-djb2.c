bddb8e256a4475094e97512d4e9c8157049f4049
ayyoubez <ayyoubez@gmail.com>
2024-02-02 07:32:05 -0800

    hash tables

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,19 +1,12 @@
-#include "hash_tables.h"
-
-/**
-* hash_djb2 - djb2 algorithm
-* @str: string hash
-* Return: calculate the array string
-*/
-
-unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long int array;
-	int t;
+    unsigned long int hash;
+    int c;
 
-	array = 5381;
-	while ((t = *str++))
-		array = ((array << 5) + array) + t;
-
-	return (array);
+    hash = 5381;
+    while ((c = *str++))
+    {
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
+    return (hash);
 }
